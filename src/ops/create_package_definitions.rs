924fd8e531d2ca39b8d42af963a91481a4e29394
Fedor Buyakov <fed@hd78.by>
2026-02-19 00:27:05 +0300

    ---

--- src/ops/create_package_definitions.rs
+++ src/ops/create_package_definitions.rs
@@ -5,7 +5,7 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use quick_xml::Writer;
+use quick_xml::{Writer, events::BytesText};
 
 use crate::config::ProjectConfig;
 
@@ -40,7 +40,58 @@ where
     writer
         .create_element("AssetPackage")
         .with_attribute(("Version", "0.1.0"))
-        .write_empty()?;
+        .write_inner_content(|w| {
+            w.create_element("ItemSettings").write_inner_content(|w| {
+                w.create_element("ContentType")
+                    .write_text_content(BytesText::new("SCENERY"))?;
+                w.create_element("Title")
+                    .write_text_content(BytesText::new(
+                        format!(
+                            "{} FSLTL Static (for {})",
+                            config.icao().to_owned().to_uppercase(),
+                            config.vendor()
+                        )
+                        .as_str(),
+                    ))?;
+                w.create_element("Creator")
+                    .write_text_content(BytesText::new(config.author()))?;
+                Ok(())
+            })?;
+
+            w.create_element("Flags").write_inner_content(|w| {
+                w.create_element("VisibleInStore")
+                    .write_text_content(BytesText::new("false"))?;
+                w.create_element("CanBeReferenced")
+                    .write_text_content(BytesText::new("false"))?;
+                Ok(())
+            })?;
+
+            w.create_element("PackageOrderHint")
+                .write_text_content(BytesText::new("CUSTOM_AIRPORT_PATCH"))?;
+
+            w.create_element("AssetGroups").write_inner_content(|w| {
+                w.create_element("AssetGroup")
+                    .with_attribute(("Name", "static"))
+                    .write_inner_content(|w| {
+                        w.create_element("Type")
+                            .with_attribute(("Version", "0"))
+                            .write_text_content(BytesText::new("BGL"))?;
+                        w.create_element("Flags").write_inner_content(|w| {
+                            w.create_element("FSXCompatibility")
+                                .write_text_content(BytesText::new("false"))?;
+                            Ok(())
+                        })?;
+                        w.create_element("AssetDir")
+                            .write_text_content(BytesText::new("PackageSources\\Scenery\\"))?;
+                        w.create_element("OutputDir")
+                            .write_text_content(BytesText::new("Scenery\\"))?;
+                        Ok(())
+                    })?;
+                Ok(())
+            })?;
+
+            Ok(())
+        })?;
 
     Ok(())
 }
